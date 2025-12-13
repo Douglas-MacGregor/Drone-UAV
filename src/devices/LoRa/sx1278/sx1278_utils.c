746f0b4cee6d549875789bf5fb4fae76cd0a649a
Douglas MacGregor <doug.macgregor04@gmail.com>
2025-12-13 12:54:52 -0800

    tryng to fix lora sx1278 inconsistences

--- src/devices/LoRa/sx1278/sx1278_utils.c
+++ src/devices/LoRa/sx1278/sx1278_utils.c
@@ -260,6 +260,7 @@ int set_stdby_mode(int spi_handle)
     {
         return n;
     }
+    fprintf(stdout, "Mode %.2X\n", mode);
     n = poll_reg(spi_handle, REG_OPMODE, (uint8_t)0x07, mode, 1000, 1000);
     return n;
 }
@@ -336,7 +337,6 @@ int poll_reg(int spi_handle, uint8_t reg_address, uint8_t mask, uint8_t expected
 
         if ((reg_value & mask) == (expected_value & mask))
         {
-            fprintf(stderr, "poll_reg: reg 0x%02X matched expected value 0x%02X (got 0x%02X)\n", reg_address, expected_value, reg_value);
             return 0; // Success
         }
         usleep(delay_us);
