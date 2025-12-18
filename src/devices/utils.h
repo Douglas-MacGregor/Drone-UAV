adb296f6fdb495cb83e6b214553c63a3e5e445e9
Douglas MacGregor <doug.macgregor04@gmail.com>
2025-12-18 12:49:11 -0800

    made iic init and started on mpu6050 read

--- src/devices/utils.h
+++ src/devices/utils.h
@@ -8,6 +8,6 @@ int init_spi();
 void close_spi(int spi_handle);
 int init_gpio();
 void close_gpio();
-int init_i2c(u_int8_t I2C_BUS, u_int8_t I2C_ADDRESS);
+int init_i2c(uint8_t I2C_BUS, uint8_t I2C_ADDRESS);
 void close_i2c(int i2c_handle);
 #endif // DEVICES_UTILS_H
