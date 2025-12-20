4eb3a17069d6904ec268a70fe9e7697faebda600
Douglas MacGregor <doug.macgregor04@gmail.com>
2025-12-19 21:40:58 -0800

    fluhsed out device functions and self test

--- src/devices/IMU/MPU-6050/mpu6050_device.c
+++ src/devices/IMU/MPU-6050/mpu6050_device.c
@@ -168,6 +168,17 @@ int mpu6050_self_test(void *self)
     float accel_result_z = (accel_diff_z / factory_trim_accel_z) * 100.0f;
 
     fprintf(stderr, "MPU6050 Self-Test Results:\n");
+    fprintf(stderr, "gyro self test values X: %.2f%%, Y: %.2f%%, Z: %.2f%%\n", gyro_bias_self_test.x, gyro_bias_self_test.y, gyro_bias_self_test.z);
+    fprintf(stderr, "acc self test values X: %.2f%%, Y: %.2f%%, Z: %.2f%%\n", accel_bias_self_test.x, accel_bias_self_test.y, accel_bias_self_test.z);
+
+    fprintf(stderr, "\n");
+
+    fprintf(stderr, "Factory Trim Values:\n");
+    fprintf(stderr, "Gyro X: %.2f, Gyro Y: %.2f, Gyro Z: %.2f\n", factory_trim_gyro_x, factory_trim_gyro_y, factory_trim_gyro_z);
+    fprintf(stderr, "Accel X: %.2f, Accel Y: %.2f, Accel Z: %.2f\n", factory_trim_accel_x, factory_trim_accel_y, factory_trim_accel_z);
+
+    fprintf(stderr, "\n");
+
     fprintf(stderr, "Gyro X: %.2f%%, Gyro Y: %.2f%%, Gyro Z: %.2f%%\n", gyro_result_x, gyro_result_y, gyro_result_z);
     fprintf(stderr, "Accel X: %.2f%%, Accel Y: %.2f%%, Accel Z: %.2f%%\n", accel_result_x, accel_result_y, accel_result_z);
     if (gyro_result_x < -14.0f || gyro_result_x > 14.0f ||
