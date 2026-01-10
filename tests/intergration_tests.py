009c00dcef73435aa8866c0cdad8565ccf6976b1
Douglas MacGregor <doug.macgregor04@gmail.com>
2026-01-09 19:04:34 -0800

    fixing syntax errors

--- tests/intergration_tests.py
+++ tests/intergration_tests.py
@@ -2,9 +2,9 @@ import unittest
 import subprocess
 import config
 
-@unittest.skipUnless(RUN_INTEGRATION_TESTS, "Skipping integration tests.")
+@unittest.skipUnless(config.RUN_INTEGRATION_TESTS, "Skipping integration tests.")
 class IntegrationTests(unittest.TestCase):
-    @unittest.skipUnless(RUN_MPU6050_I2C_TEST, "Skipping MPU-6050 I2C connection test.")
+    @unittest.skipUnless(config.RUN_MPU6050_I2C_TEST, "Skipping MPU-6050 I2C connection test.")
     def test_mpu6050_i2c_connection(self):
         """Test MPU-6050 I2C connection."""
         result = subprocess.run(["i2cdetect", "-y", "1"], capture_output=True, text=True, shell=False, check=False)
