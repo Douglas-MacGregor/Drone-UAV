5fa0eeb8c11a75bdcad0e5d1f0bca5584d46deaf
Douglas MacGregor <doug.macgregor04@gmail.com>
2026-01-09 19:25:35 -0800

    working on python tests

--- tests/device_tests.py
+++ tests/device_tests.py
@@ -43,6 +43,59 @@ class DeviceTests(unittest.TestCase):
             0,
             f"MPU-6050 unit tests failed:\n{run.stdout}\n{run.stderr}"
         )
+
+        subprocess.run(
+            ["make", "clean"],
+            cwd="devices",
+            capture_output=True,
+            text=True
+        )
         print(run.stdout)
 
+    @unittest.skipUnless(config.RUN_SX1278_BUILD_TEST, "Skipping device build test.")
+    def test_sx1278_build(self):
+        """Test building the SX1278 unittest code."""
+        result = subprocess.run("make unittest_sx1278", capture_output=True, text=True, shell=True, check=False, cwd="devices")
+        self.assertEqual(result.returncode, 0, "SX1278 build output does not match expected output.")
+        result = subprocess.run("clean", capture_output=True, text=True, shell=True, check=False, cwd="devices")
+    
+    @unittest.skipUnless(config.RUN_SX1278_UNITTESTS, "Skipping SX1278 unit tests.")
+    def test_sx1278_unittests(self):
+        """Run SX1278 Unity unit tests."""
+
+        # Build
+        build = subprocess.run(
+            ["make", "unittest_sx1278"],
+            cwd="devices",
+            capture_output=True,
+            text=True
+        )
+
+        self.assertEqual(
+            build.returncode,
+            0,
+            f"SX1278 unit test build failed:\n{build.stdout}\n{build.stderr}"
+        )
+
+        # Run Unity tests
+        run = subprocess.run(
+            ["./unittest_sx1278"],
+            cwd="devices",
+            capture_output=True,
+            text=True
+        )
+
+        self.assertEqual(
+            run.returncode,
+            0,
+            f"SX1278 unit tests failed:\n{run.stdout}\n{run.stderr}"
+        )
+
+        subprocess.run(
+            ["make", "clean"],
+            cwd="devices",
+            capture_output=True,
+            text=True
+        )
+        print(run.stdout)
 
