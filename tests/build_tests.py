70eb8fbbff86aed791f80552601bc6132c2ebba3
Douglas MacGregor <doug.macgregor04@gmail.com>
2026-01-16 12:57:35 -0800

    restructed the test files to use cmake

---
+++ tests/build_tests.py
@@ -0,0 +1,10 @@
+import unittest
+import configs.config as config
+import subprocess
+
+@unittest.skipUnless(config.RUN_BUILD_TESTS, "Skipping build tests.")
+class BuildTests(unittest.TestCase):
+    @unittest.skipUnless(config.RUN_RASPBERRUY_PI_BUILD_TEST, "Skipping Raspberry Pi build test.")
+    def test_build_rasp_process(self):
+        result = subprocess.run( "cmake -B build-rpi -DCMAKE_TOOLCHAIN_FILE=toolchains/raspberry_pi.cmake", capture_output=True, text=True, shell=True, check=False, cwd="../")
+        self.assertEqual(result.returncode, 0, "Raspberry Pi build process failed.")
