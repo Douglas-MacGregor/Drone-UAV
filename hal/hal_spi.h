60180bc8aeae06ea863b52c6d9a2525cac5bae9b
Douglas MacGregor <doug.macgregor04@gmail.com>
2026-01-15 22:00:26 -0800

    linker fix from claude

--- hal/hal_spi.h
+++ hal/hal_spi.h
@@ -5,7 +5,7 @@
 
 typedef struct
 {
-    int (*init_spi)(uint8_t SPI_BUS, uint8_t SPI_MODE, uint32_t SPI_SPEED);
+    int (*init_spi)(uint8_t spi_bus, uint8_t spi_mode, uint32_t spi_speed);
     int (*close_spi)(int spi_handle);
     int (*write_spi)(int spi_handle, uint8_t *tx_buffer, int length);
     int (*read_spi)(int spi_handle, uint8_t *rx_buffer, int length, uint8_t register_address);
