d4b3555d863c50ac993712fd64e5de9595e91ed0
Douglas MacGregor <doug.macgregor04@gmail.com>
2026-01-20 12:28:58 -0800

    trying test runner

--- tests/commandline_tests.py
+++ tests/commandline_tests.py
@@ -29,7 +29,7 @@ class CommandLineTests(unittest.TestCase):
             expected_output = f.read()
         self.assertEqual(result.stdout.strip(), expected_output.strip(), "I2C interface is not enabled as expected.")
 
-    @unittest.skipUnless(config.RUN_SPI_TEST, "Skipping SPI enabled test.")
+    @unittest.skipUnless(config.RUN_SPI_TEST_RASP, "Skipping SPI enabled test.")
     def test_spi_enabled(self):
         """Test if SPI is enabled."""
         result = subprocess.run("ls /dev/spi*", capture_output=True, text=True, shell=True, check=False)
