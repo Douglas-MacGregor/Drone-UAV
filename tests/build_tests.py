2f772e1cf80378ef285c42abf9ce07a82f7bb0dd
Douglas MacGregor <doug.macgregor04@gmail.com>
2026-01-20 12:36:40 -0800

    trying test runner

--- tests/build_tests.py
+++ tests/build_tests.py
@@ -6,5 +6,6 @@ import subprocess
 class BuildTests(unittest.TestCase):
     @unittest.skipUnless(config.RUN_RASPBERRUY_PI_BUILD_TEST, "Skipping Raspberry Pi build test.")
     def test_build_rasp_process(self):
+        """Test the Raspberry Pi build process."""
         result = subprocess.run( "cmake -B build-rpi -DCMAKE_TOOLCHAIN_FILE=toolchains/raspberry_pi.cmake", capture_output=True, text=True, shell=True, check=False, cwd="../")
         self.assertEqual(result.returncode, 0, "Raspberry Pi build process failed.")
