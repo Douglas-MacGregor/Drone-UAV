7cb41d1e8237bd34dbf5bee8796ff0580d4a81ef
Douglas MacGregor <doug.macgregor04@gmail.com>
2026-01-23 18:52:02 -0800

    making mock tx and rx test scripts

--- apps/hardware_test/tx.c
+++ apps/hardware_test/tx.c
@@ -11,7 +11,13 @@ extern HAL_SPI hal_spi;
 
 int main()
 {
-    hal_gpio.init_gpio();
+    int n = hal_gpio.init_gpio();
+    if (n < 0)
+    {
+        fprintf(stderr, "Failed to initialize GPIO\n");
+        return -1;
+    }
+    usleep(100000); // Sleep for 100ms to allow GPIO to stabilize
     int spi_hanlde = hal_spi.init_spi(0, 0, 100000);
     char data[] = "Hello, SX1278!";
     sx1278_Device device = create_sx1278_device(spi_hanlde, 26);
@@ -26,5 +32,10 @@ int main()
         printf("Failed to send data\n");
     }
     usleep(100000); // Sleep for 100ms
+    device.vtable->close(&device);
+    destroy_sx1278_device(&device);
+    hal_spi.close_spi(spi_hanlde);
+    hal_gpio.close_gpio();
+
     return 0;
 }
